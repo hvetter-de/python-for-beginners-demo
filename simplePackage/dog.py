20980a4be9a7303b547939ec06cd7b418fe5eb6d
Holger Vetter <h.vetter@outlook.com>
2020-06-26 17:06:33 +0200

    Add files via upload

---
+++ simplePackage/dog.py
@@ -0,0 +1,17 @@
+class dog:
+    '''class from https://docs.python.org/3/tutorial/classes.html'''
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+class cat:
+    '''class from https://docs.python.org/3/tutorial/classes.html'''
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
---
+++ simplePackage/simpleMath.py
@@ -0,0 +1,27 @@
+class simpleMath:
+
+    def __init__(self, value):
+        self.number = value
+
+    # Static methods doesn't have a 'self' parameter.
+    @staticmethod
+    def staticSumNumbers(number1, number2):
+        '''
+        Sum two numbers and return the result.
+        '''
+        sum = number1 + number2
+        return sum
+
+    # This is a comment
+    def getNumber(self):
+        '''
+        Returns the value from the constructor.
+        '''
+        return self.number
+
+    def sumNumbers(self, number2):
+        '''
+        Sum two numbers and return the result.
+        '''
+        sum = self.number + number2
+        return sum
